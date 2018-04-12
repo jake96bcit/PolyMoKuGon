fdb5d103c5012978e879e70fcfdf57922843e6b5
Vinh Le <vinhle11196@gmail.com>
2018-04-12 13:37:07 -0700

    Debugging.

--- gameInit.js
+++ gameInit.js
@@ -14,22 +14,6 @@ function init() {
     init_game_array();
 }
 
-function init_game_array() {
-    for (var j=0; j<=18; j++) {
-        game_state[j] = new Array();
-        for (var i = 0; i <= 22; i++) {
-            game_state[j][i] = 0;
-        }
-    }
-}
-
-function polygon_maker(x, y, size, color) {
-    // Drawing a polygon
-    var polygon = new createjs.Shape();
-    polygon.graphics.beginFill(color).drawPolyStar(x, y, size, 6, 0, 30);
-    return polygon;
-}
-
 function next_move() {
     if (turn == 0) return;
     if (turn == game_log.length) stage.removeChild(stage.getChildByName("win_line"));
@@ -39,12 +23,12 @@ function next_move() {
 }
 
 function place_move(player, x, y) {
-    game_state[x+4][y+4] = player;
+    game_state[x + 4][y + 4] = player;
     draw_game_state();
 }
 
 function remove_move() {
-    game_state[x+4][y+4] = 0;
+    game_state[x + 4][y + 4] = 0;
     draw_game_state();
 }
 
@@ -52,14 +36,14 @@ function draw_game_state() {
     stage.removeChild(stage.getChildByName("game_state_container"));
     var container = new createjs.Container();
     container.name = "game_state_container";
-    for (j=0; j<=18; j++) {
-        for (i=0; i<=22; i++) {
-            if (game_state[j][i] != 0) {
-                var color = game_state[j][i] == 1 ? "#000000" : "#ffffff";
-                if (j%2 == 0) {
-                    container.addChild(polygon_maker(40 + 54 * (i-3), 35 + (j-4)/2 * 96, 15, color));
+    for ( var i = 0; i <= 18; i++) {
+        for (var j = 0; j <= 22; j++) {
+            if (game_state[i][j] != 0) {
+                var color = game_state[i][j] == 1 ? "#000000" : "#ffffff";
+                if (i % 2 == 0) {
+                    container.addChild(polygon_maker(40 + 54 * (j-3), 35 + (i-4)/2 * 96, 15, color));
                 } else {
-                    container.addChild(polygon_maker(67 + 54 * (i-3), 83 + (j-5)/2 * 96, 15, color));
+                    container.addChild(polygon_maker(67 + 54 * (j-3), 83 + (i-5)/2 * 96, 15, color));
                 }
             }
         }
@@ -68,20 +52,28 @@ function draw_game_state() {
     stage.update();
 }
 
+function init_game_array() {
+    for (var i = 0; i <= 18; i++) {
+        game_state[i] = new Array();
+        for (var j = 0; j <= 22; j++) {
+            game_state[i][j] = 0;
+        }
+    }
+}
+
 function draw_game_table(stage) {
     // Drawing 160 polygon cells
     var polygon;
-    for (j=0; j<=5; j++) {
-        for (i=1; i<=15; i++) {
-            polygon = polygon_maker(40 + 54 * i, 35 + j * 96, 30, "#a98307");
-            polygon = polygon_maker(40 + 54 * i, 35 + j * 96, 30, "#a98307");
-            polygon.addEventListener("click", move_click(j*2, i-1));
+    for (var i = 0; i <= 5; i++) {
+        for (var j = 1; j <= 15; j++) {
+            polygon = polygon_maker(40 + 54 * j, 35 + i * 96, 30, "#a98307");
+            polygon.addEventListener("click", move_click(i * 2, j - 1));
             stage.addChild(polygon);
         }
-        if (j != 5) {
-            for (i=1; i<=14; i++) {
-                polygon = polygon_maker(67 + 54 * i, 83 + j * 96, 30, "#a98307");
-                polygon.addEventListener("click", move_click(j*2+1, i-1));
+        if (i != 5) {
+            for ( var j = 1; j <= 14; j++) {
+                polygon = polygon_maker(67 + 54 * j, 83 + i * 96, 30, "#a98307");
+                polygon.addEventListener("click", move_click(i * 2 + 1, j - 1));
                 stage.addChild(polygon);
             }
         }
@@ -100,4 +92,12 @@ function move_click(j, i) {
         current_player = current_player == 1 ? 2 : 1;
         check_game_over();
     }
+}
+
+
+function polygon_maker(x, y, size, color) {
+    // Drawing a polygon
+    var polygon = new createjs.Shape();
+    polygon.graphics.beginFill(color).drawPolyStar(x, y, size, 6, 0, 30);
+    return polygon;
 }
