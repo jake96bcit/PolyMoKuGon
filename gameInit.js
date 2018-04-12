fadd81f978af5fa874b715d6cf734d44031c02b1
Vinh Le <vinhle11196@gmail.com>
2018-04-12 13:18:59 -0700

    Adding a click function which change the color of the cell to either black or white depends on the player turn(state).

--- gameInit.js
+++ gameInit.js
@@ -1,3 +1,9 @@
+var stage;
+var game_state = new Array();
+var turn = 0;
+var current_player = 1;
+var game_log = [];
+
 $(function(){
     init();
 });
@@ -5,12 +11,13 @@ $(function(){
 function init() {
     stage = new createjs.Stage("gameCanvas");
     draw_game_table(stage); // Draw the table
+    init_game_array();
 }
 
 function init_game_array() {
-    for (j=0; j<=18; j++) {
+    for (var j=0; j<=18; j++) {
         game_state[j] = new Array();
-        for (i=0; i<=22; i++) {
+        for (var i = 0; i <= 22; i++) {
             game_state[j][i] = 0;
         }
     }
@@ -23,22 +30,30 @@ function polygon_maker(x, y, size, color) {
     return polygon;
 }
 
-function move_click(j, i) {
-    return function(event) {
-        if (game_log.indexOf(j+" "+i) != -1){
-            return
-        } else {
-            game_log.push(j+" "+i);
-        }
-        place_move(current_player, j, i);
-        current_player = current_player == 1 ? 2 : 1;
-        check_game_over();
-    }
-}
 
 function place_move(player, x, y) {
     game_state[x+4][y+4] = player;
     draw_game_state();
+}
+
+function draw_game_state() {
+    stage.removeChild(stage.getChildByName("game_state_container"));
+    var container = new createjs.Container();
+    container.name = "game_state_container";
+    for (j=0; j<=18; j++) {
+        for (i=0; i<=22; i++) {
+            if (game_state[j][i] != 0) {
+                var color = game_state[j][i] == 1 ? "#000000" : "#ffffff";
+                if (j%2 == 0) {
+                    container.addChild(polygon_maker(40 + 54 * (i-3), 35 + (j-4)/2 * 96, 15, color));
+                } else {
+                    container.addChild(polygon_maker(67 + 54 * (i-3), 83 + (j-5)/2 * 96, 15, color));
+                }
+            }
+        }
+    }
+    stage.addChild(container);
+    stage.update();
 }
 
 function draw_game_table(stage) {
@@ -54,10 +69,23 @@ function draw_game_table(stage) {
         if (j != 5) {
             for (i=1; i<=14; i++) {
                 polygon = polygon_maker(67 + 54 * i, 83 + j * 96, 30, "#a98307");
-
+                polygon.addEventListener("click", move_click(j*2+1, i-1));
                 stage.addChild(polygon);
             }
         }
     }
     stage.update();
+}
+
+function move_click(j, i) {
+    return function(event) {
+        if (game_log.indexOf(j+" "+i) != -1){
+            return
+        } else {
+            game_log.push(j+" "+i);
+        }
+        place_move(current_player, j, i);
+        current_player = current_player == 1 ? 2 : 1;
+        check_game_over();
+    }
 }
