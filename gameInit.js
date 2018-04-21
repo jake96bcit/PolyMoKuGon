5c3b08ed1c13a1afc9e5bb9c6d7ba476d64d1cf1
Vinh Le <vinhle11196@gmail.com>
2018-04-21 16:11:10 -0700

    Marking the winning line.

--- gameInit.js
+++ gameInit.js
@@ -1,5 +1,5 @@
 var stage;
-var game_state = new Array();
+var game_state = Array();
 var turn = 0;
 var current_player = 1;
 var game_log = [];
@@ -15,50 +15,50 @@ function init() {
 }
 
 function check_game_over() {
-    var win = new createjs.Shape();
-    win.name = "win";
-    win.graphics.setStrokeStyle(3);
-    win.graphics.beginStroke('#e74c3c');
-    for (var i = 0; i <= 14; i++) {
-        for (var j = 0; j <= 18; j++) {
-            if ([game_state[i][j - 4], game_state[i][j - 3], game_state[i][j - 2], game_state[i][j - 1], game_state[i][j]].check_same()
-                || [game_state[i][j + 1], game_state[i][j + 2], game_state[i][j + 3], game_state[i][j + 4], game_state[i][j]].check_same()
-                || [game_state[j][i + 1], game_state[j][i + 2], game_state[j][i - 2], game_state[j][i - 1], game_state[j][i]].check_same()
-                || [game_state[j][i + 1], game_state[j][i + 2], game_state[j][i + 3], game_state[j][i - 1], game_state[j][i]].check_same()
-                || [game_state[j][i + 1], game_state[j][i + 2], game_state[j][i + 3], game_state[j][i + 4], game_state[j][i]].check_same()) {
-                if (j % 2 == 0) {
-                    win.graphics.moveTo(40 + 54 * (i - 3) - 30, 35 + (j - 4) / 2 * 96).lineTo(40 + 54 * (i - 3) + 30, 35 + (j - 4) / 2 * 96);
+    var win_line = new createjs.Shape();
+    win_line.name = "win_line";
+    win_line.graphics.setStrokeStyle(3);
+    win_line.graphics.beginStroke("#e74c3c");
+    for (var j=4; j<=14; j++) {
+        for (var i=4; i<=18; i++) {
+            if ([game_state[j][i-4],game_state[j][i-3],game_state[j][i-2],game_state[j][i-1],game_state[j][i]].check_same()
+                ||[game_state[j][i+1],game_state[j][i-3],game_state[j][i-2],game_state[j][i-1],game_state[j][i]].check_same()
+                ||[game_state[j][i+1],game_state[j][i+2],game_state[j][i-2],game_state[j][i-1],game_state[j][i]].check_same()
+                ||[game_state[j][i+1],game_state[j][i+2],game_state[j][i+3],game_state[j][i-1],game_state[j][i]].check_same()
+                ||[game_state[j][i+1],game_state[j][i+2],game_state[j][i+3],game_state[j][i+4],game_state[j][i]].check_same()) {
+                if (j%2 === 0) {
+                    win_line.graphics.moveTo(40 + 54 * (i-3) - 30, 35 + (j-4)/2 * 96).lineTo(40 + 54 * (i-3) + 30, 35 + (j-4)/2 * 96);
                 } else {
-                    win.graphics.moveTo(67 + 54 * (i - 3) - 30, 83 + (j - 5) / 2 * 96).lineTo(67 + 54 * (i - 3) + 30, 83 + (j - 5) / 2 * 96);
+                    win_line.graphics.moveTo(67 + 54 * (i-3) - 30, 83 + (j-5)/2 * 96).lineTo(67 + 54 * (i-3) + 30, 83 + (j-5)/2 * 96);
                 }
             }
-            var ya = j % 2 == 0 ? [i - 2, i - 2, i - 1, i - 1, i, i + 1, i + 1, i + 2] : [i - 2, i - 1, i - 1, i, i + 1, i + 1, i + 2, i + 2];
-            if ([game_state[j - 4][ya[0]], game_state[j - 3][ya[1]], game_state[j - 2][ya[2]], game_state[j - 1][ya[3]], game_state[j][i]].check_same()
-                || [game_state[j + 1][ya[4]], game_state[j - 3][ya[1]], game_state[j - 2][ya[2]], game_state[j - 1][ya[3]], game_state[j][i]].check_same()
-                || [game_state[j + 1][ya[4]], game_state[j + 2][ya[5]], game_state[j - 2][ya[2]], game_state[j - 1][ya[3]], game_state[j][i]].check_same()
-                || [game_state[j + 1][ya[4]], game_state[j + 2][ya[5]], game_state[j + 3][ya[6]], game_state[j - 1][ya[3]], game_state[j][i]].check_same()
-                || [game_state[j + 1][ya[4]], game_state[j + 2][ya[5]], game_state[j + 3][ya[6]], game_state[j + 4][ya[7]], game_state[j][i]].check_same()) {
-                if (j % 2 == 0) {
-                    win.graphics.moveTo(40 + 54 * (i - 3) - 17, 35 + (j - 4) / 2 * 96 - 30).lineTo(40 + 54 * (i - 3) + 17, 35 + (j - 4) / 2 * 96 + 30);
+            var ya = j%2 === 0 ? [i-2,i-2,i-1,i-1,i,i+1,i+1,i+2] : [i-2,i-1,i-1,i,i+1,i+1,i+2,i+2];
+            if ([game_state[j-4][ya[0]],game_state[j-3][ya[1]],game_state[j-2][ya[2]],game_state[j-1][ya[3]],game_state[j][i]].check_same()
+                ||[game_state[j+1][ya[4]],game_state[j-3][ya[1]],game_state[j-2][ya[2]],game_state[j-1][ya[3]],game_state[j][i]].check_same()
+                ||[game_state[j+1][ya[4]],game_state[j+2][ya[5]],game_state[j-2][ya[2]],game_state[j-1][ya[3]],game_state[j][i]].check_same()
+                ||[game_state[j+1][ya[4]],game_state[j+2][ya[5]],game_state[j+3][ya[6]],game_state[j-1][ya[3]],game_state[j][i]].check_same()
+                ||[game_state[j+1][ya[4]],game_state[j+2][ya[5]],game_state[j+3][ya[6]],game_state[j+4][ya[7]],game_state[j][i]].check_same()) {
+                if (j%2 === 0) {
+                    win_line.graphics.moveTo(40 + 54 * (i-3) - 17, 35 + (j-4)/2 * 96 - 30).lineTo(40 + 54 * (i-3) + 17, 35 + (j-4)/2 * 96 + 30);
                 } else {
-                    win.graphics.moveTo(67 + 54 * (i - 3) - 17, 83 + (j - 5) / 2 * 96 - 30).lineTo(67 + 54 * (i - 3) + 17, 83 + (j - 5) / 2 * 96 + 30);
+                    win_line.graphics.moveTo(67 + 54 * (i-3) - 17, 83 + (j-5)/2 * 96 - 30).lineTo(67 + 54 * (i-3) + 17, 83 + (j-5)/2 * 96 + 30);
                 }
             }
-            ya = j % 2 == 0 ? [i + 2, i + 1, i + 1, i, i - 1, i - 1, i - 2, i - 2] : [i + 2, i + 2, i + 1, i + 1, i, i - 1, i - 1, i - 2];
-            if ([game_state[j - 4][ya[0]], game_state[j - 3][ya[1]], game_state[j - 2][ya[2]], game_state[j - 1][ya[3]], game_state[j][i]].check_same()
-                || [game_state[j + 1][ya[4]], game_state[j - 3][ya[1]], game_state[j - 2][ya[2]], game_state[j - 1][ya[3]], game_state[j][i]].check_same()
-                || [game_state[j + 1][ya[4]], game_state[j + 2][ya[5]], game_state[j - 2][ya[2]], game_state[j - 1][ya[3]], game_state[j][i]].check_same()
-                || [game_state[j + 1][ya[4]], game_state[j + 2][ya[5]], game_state[j + 3][ya[6]], game_state[j - 1][ya[3]], game_state[j][i]].check_same()
-                || [game_state[j + 1][ya[4]], game_state[j + 2][ya[5]], game_state[j + 3][ya[6]], game_state[j + 4][ya[7]], game_state[j][i]].check_same()) {
-                if (j % 2 == 0) {
-                    win.graphics.moveTo(40 + 54 * (i - 3) + 17, 35 + (j - 4) / 2 * 96 - 30).lineTo(40 + 54 * (i - 3) - 17, 35 + (j - 4) / 2 * 96 + 30);
+            ya = j%2 === 0 ? [i+2,i+1,i+1,i,i-1,i-1,i-2,i-2] : [i+2,i+2,i+1,i+1,i,i-1,i-1,i-2];
+            if ([game_state[j-4][ya[0]],game_state[j-3][ya[1]],game_state[j-2][ya[2]],game_state[j-1][ya[3]],game_state[j][i]].check_same()
+                ||[game_state[j+1][ya[4]],game_state[j-3][ya[1]],game_state[j-2][ya[2]],game_state[j-1][ya[3]],game_state[j][i]].check_same()
+                ||[game_state[j+1][ya[4]],game_state[j+2][ya[5]],game_state[j-2][ya[2]],game_state[j-1][ya[3]],game_state[j][i]].check_same()
+                ||[game_state[j+1][ya[4]],game_state[j+2][ya[5]],game_state[j+3][ya[6]],game_state[j-1][ya[3]],game_state[j][i]].check_same()
+                ||[game_state[j+1][ya[4]],game_state[j+2][ya[5]],game_state[j+3][ya[6]],game_state[j+4][ya[7]],game_state[j][i]].check_same()) {
+                if (j%2 === 0) {
+                    win_line.graphics.moveTo(40 + 54 * (i-3) + 17, 35 + (j-4)/2 * 96 - 30).lineTo(40 + 54 * (i-3) - 17, 35 + (j-4)/2 * 96 + 30);
                 } else {
-                    win.graphics.moveTo(67 + 54 * (i - 3) + 17, 83 + (j - 5) / 2 * 96 - 30).lineTo(67 + 54 * (i - 3) - 17, 83 + (j - 5) / 2 * 96 + 30);
+                    win_line.graphics.moveTo(67 + 54 * (i-3) + 17, 83 + (j-5)/2 * 96 - 30).lineTo(67 + 54 * (i-3) - 17, 83 + (j-5)/2 * 96 + 30);
                 }
             }
         }
     }
-    stage.addChild(win);
+    stage.addChild(win_line);
     stage.update();
 }
 (function() {
@@ -74,9 +74,17 @@ function check_game_over() {
 
 
 function next_move() {
+    if (turn === game_log.length) return;
+    turn ++;
+    place_move(current_player, game_log[turn-1][0], game_log[turn-1][1]);
+    if (turn === game_log.length) check_game_over();
+    current_player = current_player === 1 ? 2 : 1;
+}
+
+function prev_move() {
     if (turn === 0) return;
     if (turn === game_log.length) stage.removeChild(stage.getChildByName("win_line"));
-    turn--;
+    turn --;
     remove_move(game_log[turn][0], game_log[turn][1]);
     current_player = current_player === 1 ? 2 : 1;
 }
@@ -112,9 +120,9 @@ function draw_game_state() {
 }
 
 function init_game_array() {
-    for (var i = 0; i <= 23; i++) {
-        game_state[i] = new Array();
-        for (var j = 0; j <= 23; j++) {
+    for (var i = 0; i <= 18; i++) {
+        game_state[i] = Array();
+        for (var j = 0; j <= 22; j++) {
             game_state[i][j] = 0;
         }
     }
@@ -123,16 +131,16 @@ function init_game_array() {
 function draw_game_table(stage) {
     // Drawing 160 polygon cells
     var polygon;
-    for (var i = 0; i <= 6; i++) {
-        for (var j = 1; j <= 19; j++) {
+    for (var i = 0; i <= 5; i++) {
+        for (var j = 1; j <= 15; j++) {
             polygon = polygon_maker(40 + 54 * j, 35 + i * 96, 30, "#a98307");
             polygon.addEventListener("click", move_click(i * 2, j - 1));
             stage.addChild(polygon);
         }
-        if (i !== 6) {
-            for ( var k = 1; k <= 19; k++) {
-                polygon = polygon_maker(67 + 54 * k, 83 + i * 96, 30, "#a98307");
-                polygon.addEventListener("click", move_click(i * 2 + 1, k - 1));
+        if (i !== 5) {
+            for (var j = 1; j <= 14; j++) {
+                polygon = polygon_maker(67 + 54 * j, 83 + i * 96, 30, "#a98307");
+                polygon.addEventListener("click", move_click(i * 2 + 1, j - 1));
                 stage.addChild(polygon);
             }
         }
